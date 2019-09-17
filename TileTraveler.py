026d3eb88374cbee85dd837d09f34cdb4a05141e
Fridjon-hinn-fyrsti <gudmundufe19@ru.is>
2019-09-17 06:01:21 -0400

    reiknirit skapadur fyrir forrit

---
+++ TileTraveler.py
@@ -0,0 +1,11 @@
+#notum fall sem tekur inn x og y hnit notanda og skilar volmoguleikum
+#fallid tekur inn x og y hnit og skilar streng med attum
+#fallid gerir tedda med if setningum
+
+#geymum x og y hnit notanda
+#tokum inn input hans
+#skopun fall sem synir notanda loglegar leidir og tekur einn attir i strng "NESW"
+#berum sman input vid loglegar leidir
+#ef input er ologlegt reyna aftur ad taka inn input
+#annars uppfaera stadsetningu
+#ef stadsetning er (3,1) skila victory og stoppa forrit
